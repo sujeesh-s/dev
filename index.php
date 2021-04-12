6cff738eb4f21661bfb3322c9bdb2b9faed13754
sujeesh-s <s.sujeesh1993@yahoo.com>
2021-04-12 07:55:15 +0530

    index updated

---
+++ index.php
@@ -0,0 +1,54 @@
+<!-- <link href="https://www.fridayweekend.com/resources/css/fridayweekend.styles.css" rel="stylesheet" type="text/css" /> -->
+
+<link href="jquery.imagepicker.css" rel="stylesheet" type="text/css" />
+
+<link href="jquery-ui.min.css" rel="stylesheet" type="text/css" />
+<script src="jquery-1.11.1.min.js" ></script>
+<script src="jquery-ui.custom.min.js" ></script>
+
+<script src="jquery.imagepicker.js" ></script>
+
+
+
+<select id="icon" name="icon">
+              
+                <option value="https://www.fridayweekend.com/resources/icons/beach.png"
+                  >
+                  beach
+                </option>
+              
+                <option value="https://www.fridayweekend.com/resources/icons/bridge.png"
+                  >
+                  bridge
+                </option>
+              
+                <option value="https://www.fridayweekend.com/resources/icons/camp.png"
+                  >
+                  camp
+                </option>
+              
+                <option value="https://www.fridayweekend.com/resources/icons/camper.png"
+                  >
+                  camper
+                </option>
+              
+                <option value="https://www.fridayweekend.com/resources/icons/canu.png"
+                  >
+                  canu
+                </option>
+              
+                <option value="https://www.fridayweekend.com/resources/icons/car.png"
+                  >
+                  car
+                </option>
+              
+                
+              
+            </select>
+
+
+<script type="text/javascript">
+  jQuery(document).ready(function(){
+$("#icon").imagePicker();
+  });
+</script>
